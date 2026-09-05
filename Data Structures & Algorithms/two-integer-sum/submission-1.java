8f1de97a3bdcb1667a80683c7f2b9ae58bd74485
Ahmad Saleh <rusteze820@hotmail.com>
2026-09-04 19:34:23 -0500

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,18 @@
+/** Sept 4th, 2026
+    Two Sum
+    @author Ahmad Saleh
+    */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
